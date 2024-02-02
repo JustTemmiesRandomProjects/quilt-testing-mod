a55ca73536dffdd11bd48619fb45c8eb1ef8827e
Temmie <47639983+JustTemmie@users.noreply.github.com>
2024-02-02 16:26:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Quilt Template Mod
+
+This is my Quilt template mod thingy. It's based on the official one, if you'd like, you can use it as a template for your own mods!
---
+++ src/main/java/mom/beaver/example_mod/mixin/TitleScreenMixin.java
@@ -0,0 +1,16 @@
+package mom.beaver.example_mod.mixin;
+
+import mom.beaver.example_mod.ExampleMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class TitleScreenMixin {
+	@Inject(method = "init", at = @At("TAIL"))
+	public void onInit(CallbackInfo ci) {
+		ExampleMod.LOGGER.info("This line was printed by Temmie :3");
+	}
+}
